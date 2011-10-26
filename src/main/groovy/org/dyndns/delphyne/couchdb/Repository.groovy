18e95e64ee3a37a45ae2393db527d5a4bb8f49ab
Brian M. Carr <delphyne@gmail.com>
2011-10-26 17:59:53 -0500

    AST transformation properly mixing in repository and adding _id and _rev properties.  Repository sloppy, but find is working

--- src/main/groovy/org/dyndns/delphyne/couchdb/Repository.groovy
+++ src/main/groovy/org/dyndns/delphyne/couchdb/Repository.groovy
@@ -1,5 +1,36 @@
 package org.dyndns.delphyne.couchdb
 
-interface Repository {
+import groovy.json.JsonSlurper;
+import groovyx.net.http.HTTPBuilder;
 
+/**
+ * This class is Mixed into all classes annotated by Document in order to provide the create, update, retrieve, and 
+ *  delete methods.
+ * @author bcarr
+ *
+ */
+class Repository {
+    RepositoryConfig config
+    
+    def find(String key) {
+        find(this.class, key)
+    }
+    
+    private <T> T find(Class<T> type, String key) {
+        HTTPBuilder couch = new HTTPBuilder(config.url)
+        couch.auth.basic(config.apikey.user, config.apikey.password)
+        def response = couch.get(path: "${type.simpleName.toLowerCase()}/${key}")
+        JsonSlurper slurper = new JsonSlurper()
+        type.newInstance(slurper.parse(response))
+    }
+}
+
+class RepositoryConfig {
+    String url
+    APIKey apikey
+    
+    class APIKey {
+        String user
+        String password
+    }
 }
---
+++ src/test/resources/repository-config.groovy
@@ -0,0 +1,5 @@
+url = 'http://localhost:5984'
+apikey {
+    user = 'k8spelling_test_account'
+    password = 'k8spelling_test_account_password'
+}
